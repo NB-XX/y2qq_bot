99f89be4d2bd870090eb7d8bb501f32a9ab883a6
NB-XX <53631369+NB-XX@users.noreply.github.com>
2023-05-07 15:22:30 +0800

    Update restream.py add rtmp key

--- restream.py
+++ restream.py
@@ -1,24 +1,28 @@
 import subprocess
-import streamlink
+import yt_dlp
 
 
-def start_live(url):
-    streams = streamlink.streams('https://www.youtube.com/watch?v='+url)
-    if 'best' in streams:
-        push_m3u8_to_rtmp(streams['best'].url)
-    print('获取失败')
+def start_live(video_id, user_key):
+    try:
+        ydl = yt_dlp.YoutubeDL()
+        info_dict = ydl.extract_info(
+            'https://www.youtube.com/watch?v='+video_id, download=False)
+        m3u8 = info_dict['formats'][0]['url']
+        push_m3u8_to_rtmp(m3u8, user_key)
+    except Exception as e:
+        print(e)
 
 
-def push_m3u8_to_rtmp(m3u8_url):
+def push_m3u8_to_rtmp(m3u8_url, user_key):
     ffmpeg_cmd = [
         'ffmpeg',
         '-i', m3u8_url,
         '-c', 'copy',
         '-f', 'flv',
-        < rtmp >
+        f'rtmp://qqgroup.6721.livepush.ilive.qq.com/trtc_1400526639/{user_key}'
     ]
 
     try:
         subprocess.call(ffmpeg_cmd)
-    except KeyboardInterrupt:
-        pass
+    except Exception as e:
+        return e
